e4bf4923038232154178a32e18bbd4ba6bb8fb92
SHAcollision <cotyleron@proton.me>
2024-11-26 19:07:13 +0100

    Expose PubkyApp models on top level

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,11 @@ pub mod tag;
 pub mod traits;
 pub mod types;
 pub mod user;
+
+pub use bookmark::PubkyAppBookmark;
+pub use file::PubkyAppFile;
+pub use follow::PubkyAppFollow;
+pub use mute::PubkyAppMute;
+pub use post::{PostEmbed, PostKind, PubkyAppPost};
+pub use tag::PubkyAppTag;
+pub use user::{PubkyAppUser, UserLink};
