781cd5ff53e2feaa2a4c23ed91da27adf3908c9b
SHAcollision <cotyleron@proton.me>
2026-01-26 12:10:24 +0100

    increase post attachments to 4

--- src/limits.rs
+++ src/limits.rs
@@ -84,7 +84,7 @@ pub const VALIDATION_LIMITS: ValidationLimits = ValidationLimits {
     user_status_max_length: 50,
     post_short_content_max_length: 2000,
     post_long_content_max_length: 50_000,
-    post_attachments_max_count: 3,
+    post_attachments_max_count: 4,
     post_attachment_url_max_length: 200,
     post_allowed_attachment_protocols: &["pubky", "http", "https"],
     file_name_min_length: 1,
